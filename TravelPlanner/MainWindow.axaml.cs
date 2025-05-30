721422109deee37b56f3e5b0cdb07e03333faf72
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2025-05-30 20:20:21 +0200

    Zrobienie funkcjonalnośći dla dwoch buttonów

--- TravelPlanner/MainWindow.axaml.cs
+++ TravelPlanner/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Media.Imaging;
@@ -34,11 +36,52 @@ public partial class MainWindow : Window
 
     private void Button_OnClick(object? sender, RoutedEventArgs e)
     {
-        
+        string miasto = Miasto.Text;
+        if (!string.IsNullOrWhiteSpace(miasto))
+        {
+            Miasta.Items.Add(miasto);
+        }
     }
 
     private void Button_OnClick2(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        string imie = ImieNazwisko.Text;
+        string kraj = (Kraj.SelectedItem as ComboBoxItem)?.Content.ToString() ?? "";
+        
+        List<string> atrakcje = new();
+        if (MuzeaAtrakcje.IsChecked == true)
+        {
+            atrakcje.Add("Muzea");
+        }
+        if (ParkiNarodoweAtrakcje.IsChecked == true)
+        {
+            atrakcje.Add("Parki Narodowe");
+        }
+        if (ZabytkiAtrakcje.IsChecked == true)
+        {
+            atrakcje.Add("Zabytki");
+        }
+        if (RestauracjeAtrakcje.IsChecked == true)
+        {
+            atrakcje.Add("Restauracje");
+        }
+        if (GalerieSztukiAtrakcje.IsChecked == true)
+        {
+            atrakcje.Add("GalerieSztuki");
+        }
+        if (FestiwaleAtrakcje.IsChecked == true)
+        {
+            atrakcje.Add("Festiwale i koncerty");
+        }
+
+        string transport = Samolot.IsChecked == true ? "Samolot" :
+            Samochód.IsChecked == true ? "Samochód" :
+            Pociag.IsChecked == true ? "Pociag" :
+            Statek.IsChecked == true ? "Statek" : "";
+        
+        var miasta = string.Join(", ", Miasta.SelectedItems.Cast<string>());
+        
+        var SecondWindow = new SecondWindow(imie, kraj, atrakcje, transport, miasta);
+        SecondWindow.Show();
     }
 }
---
+++ TravelPlanner/SecondWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TravelPlanner;
+
+public partial class SecondWindow : Window
+{
+    public SecondWindow()
+    {
+        InitializeComponent();
+    }
+}
