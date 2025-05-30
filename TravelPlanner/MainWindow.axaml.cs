3b6d4d7113617d39803360873cec28badb3e7512
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2025-05-30 18:52:39 +0200

    uzupełnienie MainWindow.axaml

--- TravelPlanner/MainWindow.axaml.cs
+++ TravelPlanner/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TravelPlanner;
 
@@ -7,5 +8,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Kraj_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void Button_OnClick2(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
